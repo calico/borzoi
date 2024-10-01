23e49ddb38ef14a34858312883166eef232aa9fa
Johannes Linder <jlinder@cb2-jlinder.calicolabs.local>
2024-10-01 11:27:33 -0700

    Updated download/install helper scripts.

--- env_vars.sh
+++ env_vars.sh
@@ -2,8 +2,6 @@
 
 # set these variables before running the script
 LOCAL_BORZOI_PATH="/home/jlinder/borzoi"
-LOCAL_BASKERVILLE_PATH="/home/jlinder/baskerville"
-LOCAL_WESTMINSTER_PATH="/home/jlinder/westminster"
 LOCAL_USER="jlinder"
 
 # create env_vars sh scripts in local conda env
@@ -20,27 +18,17 @@ if ! [ -e $file_vars_deact ]; then
     echo '#!/bin/sh' > $file_vars_deact
 fi
 
-# append borzoi (and baskerville/westminster) env variable exports to /activate.d/env_vars.sh
+# append env variable exports to /activate.d/env_vars.sh
 echo "export BORZOI_DIR=$LOCAL_BORZOI_PATH" >> $file_vars_act
 echo 'export PATH=$BORZOI_DIR/src/scripts:$PATH' >> $file_vars_act
 echo 'export PYTHONPATH=$BORZOI_DIR/src/scripts:$PYTHONPATH' >> $file_vars_act
-
-echo "export BASKERVILLE_DIR=$LOCAL_BASKERVILLE_PATH" >> $file_vars_act
-echo 'export PATH=$BASKERVILLE_DIR/src/baskerville/scripts:$PATH' >> $file_vars_act
-echo 'export PYTHONPATH=$BASKERVILLE_DIR/src/baskerville/scripts:$PYTHONPATH' >> $file_vars_act
-
-echo "export WESTMINSTER_DIR=$LOCAL_WESTMINSTER_PATH" >> $file_vars_act
-echo 'export PATH=$WESTMINSTER_DIR/src/westminster/scripts:$PATH' >> $file_vars_act
-echo 'export PYTHONPATH=$WESTMINSTER_DIR/src/westminster/scripts:$PYTHONPATH' >> $file_vars_act
 
 echo 'export BORZOI_HG38=$BORZOI_DIR/examples/hg38' >> $file_vars_act
 echo 'export BORZOI_MM10=$BORZOI_DIR/examples/mm10' >> $file_vars_act
 
 echo "export BORZOI_CONDA=/home/$LOCAL_USER/anaconda3/etc/profile.d/conda.sh" >> $file_vars_act
 
-# append borzoi env variable unsets to /deactivate.d/env_vars.sh
-echo 'unset BASKERVILLE_DIR' >> $file_vars_deact
-echo 'unset WESTMINSTER_DIR' >> $file_vars_deact
+# append env variable unsets to /deactivate.d/env_vars.sh
 echo 'unset BORZOI_DIR' >> $file_vars_deact
 echo 'unset BORZOI_HG38' >> $file_vars_deact
 echo 'unset BORZOI_MM10' >> $file_vars_deact
